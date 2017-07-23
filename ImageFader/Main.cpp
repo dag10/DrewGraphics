1c7c4dc5ca4d54adc660f79df4711e5f374cf41c
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-07-22 22:11:37 -0700

    Added images, started vertex shader.

--- ImageFader/Main.cpp
+++ ImageFader/Main.cpp
@@ -8,9 +8,39 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+// Types
+
+struct {
+	GLuint vertex_buffer;
+	GLuint element_buffer;
+	GLuint textures[2];
+
+	// TODO: Add fields for shader objects.
+} g_resources;
+
+// Scene data
+
+static const GLfloat g_vertex_buffer_data[] = { 
+    -1.0f, -1.0f,
+     1.0f, -1.0f,
+    -1.0f,  1.0f,
+     1.0f,  1.0f,
+};
+
+static const GLushort g_element_buffer_data[] = {
+	0, 1, 2, 3,
+};
+
+// Functions
+
+// util.c
+void *read_tga(const char *filename, int *width, int *height);
+
 bool make_resources();
 void update_fade_factor();
 void render();
+GLuint make_buffer(GLenum target, const void *buffer_data, GLsizei buffer_size);
+GLuint make_texture(const char *filename);
 
 int main(int argc, char **argv)
 {
@@ -29,7 +59,7 @@ int main(int argc, char **argv)
 
     if (!make_resources()) {
 		std::cerr << "Failed to load resources." << std::endl;
-        return 1;
+		return 1;
     }
 
     glutMainLoop();
@@ -38,15 +68,93 @@ int main(int argc, char **argv)
 }
 
 bool make_resources() {
+	g_resources.vertex_buffer = make_buffer(
+		GL_ARRAY_BUFFER,
+		g_vertex_buffer_data,
+		sizeof(g_vertex_buffer_data)
+	);
+
+	g_resources.element_buffer = make_buffer(
+		GL_ELEMENT_ARRAY_BUFFER,
+		g_element_buffer_data,
+		sizeof(g_element_buffer_data)
+	);
+
+	g_resources.textures[0] = make_texture("image1.tga");
+	g_resources.textures[1] = make_texture("image2.tga");
+
+	if (!g_resources.textures[0] || !g_resources.textures[1]) {
+		return false;
+	}
+
 	return true;
 }
 
 void update_fade_factor() {
-
+	// TODO: Update fade amount.
 }
 
 void render() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers();	
+}
+
+GLuint make_buffer(
+		GLenum target, const void *buffer_data,
+		GLsizei buffer_size) {
+	GLuint buffer;
+
+	// Generate one new buffer handle.
+	glGenBuffers(1, &buffer);
+
+	// Reference this buffer through a particular target.
+	glBindBuffer(target, buffer);
+
+	// Allocate the buffer size, with a usage hint indicating
+	// that we only write the data once, and only the GPU accesses it.
+	glBufferData(target, buffer_size, buffer_data, GL_STATIC_DRAW);
+
+	return buffer;
+}
+
+GLuint make_texture(const char *filename) {
+	GLuint texture;
+	int width;
+	int height;
+
+	void *pixels = read_tga(filename, &width, &height);
+	if (!pixels) {
+		return 0;
+	}
+
+	// Generate one new texture handle.
+	glGenTextures(1, &texture);
+
+	// Reference this texture through a particular target.
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Set parameters for this texture.
+	// It will use linear interpolation, and clamp out-of-bound
+	// values to the nearest edge.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// Allocate the texture space and transfer the pixel data.
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0, // Level of detail
+		GL_RGB8, // Internal format
+		width,
+		height,
+		0, // Border
+		GL_BGR, // External format
+		GL_UNSIGNED_BYTE, // Type
+		pixels
+	);
+
+	free(pixels);
+	return texture;
 }

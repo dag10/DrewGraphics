106d74eeb896413e513d8314db27e515153db54b
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-07-27 20:27:11 -0700

    Fixed some indentation.

--- ImageFader/naive-rotation.v.glsl
+++ ImageFader/naive-rotation.v.glsl
@@ -12,22 +12,22 @@ void main()
     const float aspect = 3.0 / 4.0;
     
     const mat3 projection = mat3(
-                                 vec3(aspect, 0.0, 0.0),
-                                 vec3(   0.0, 1.0, 0.0),
-                                 vec3(   0.0, 0.0, 0.5)
-                                 );
-    
+        vec3(aspect, 0.0, 0.0),
+        vec3(   0.0, 1.0, 0.0),
+        vec3(   0.0, 0.0, 0.5)
+    );
+
     mat3 rotation = mat3(
-                         vec3(1.0,                0.0,                0.0),
-                         vec3(0.0,  cos(elapsed_time),  sin(elapsed_time)),
-                         vec3(0.0, -sin(elapsed_time),  cos(elapsed_time))
-                         );
+        vec3(1.0,                0.0,                0.0),
+        vec3(0.0,  cos(elapsed_time),  sin(elapsed_time)),
+        vec3(0.0, -sin(elapsed_time),  cos(elapsed_time))
+    );
     
     mat3 scale = mat3(
-                      vec3(1.0 / aspect, 0.0, 0.0),
-                      vec3(         0.0, 1.0, 0.0),
-                      vec3(         0.0, 0.0, 1.0)
-                      );
+        vec3(1.0 / aspect, 0.0, 0.0),
+        vec3(         0.0, 1.0, 0.0),
+        vec3(         0.0, 0.0, 1.0)
+    );
     
     vec3 projected_position = projection * rotation * scale * position.xyz;
     float perspective_factor = projected_position.z * 0.5 + 1.0;

5675832d6b532e8a2c43dc1ee91fb9cbf8d30a22
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-08-20 17:23:09 -0500

    HelloGlfw: Add keyboard input and background color.

--- HelloGlfw/src/main.cpp
+++ HelloGlfw/src/main.cpp
@@ -2,6 +2,22 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const char *WINDOW_TITLE = "Hello OpenGL";
+
+void processInput(GLFWwindow *window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void terminateWithError(const char *error) {
+  std::cerr << error << std::endl;
+  glfwTerminate();
+  exit(-1);
+}
+
 int main() {
   // Print GLFW errors to stderr.
   glfwSetErrorCallback([](int code, const char *desc) {
@@ -9,9 +25,7 @@ int main() {
   });
 
   if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW." << std::endl;
-    glfwTerminate();
-    return -1;
+    terminateWithError("Failed to initialize GLFW.");
   }
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -21,19 +35,40 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
-  if (window == NULL) {
-    std::cerr << "Failed to create GLFW window." << std::endl;
-    glfwTerminate();
-    return -1;
+  // Create window.
+  GLFWwindow *window = glfwCreateWindow(
+     WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+  if (window == nullptr) {
+    terminateWithError("Failed to create GLFW window.");
   }
   glfwMakeContextCurrent(window);
+  
+  // Load GLAD procedures.
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    terminateWithError("Failed to initialize GLAD.");
+  }
 
+  // Specify the rect to render to within the window.
+  // TODO: Update this when we detect a window resize event.
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+  // Application loop.
   while(!glfwWindowShouldClose(window)) {
+    // Process input for current frame.
+    processInput(window);
+
+    // Clear back buffer.
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Present back buffer.
     glfwSwapBuffers(window);
+
+    // Poll events for next frame.
     glfwPollEvents();
   }
 
+  glfwTerminate();
   return 0;
 }
 

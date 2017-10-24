2a6313e2601cd03894596dcd63a5ead42fe7c8f5
Drew Gottlieb <gottlieb.drew@gmail.com>
2017-10-24 02:20:20 -0400

    Fixed normal map having an inverted bitangent.

--- assets/shaders/includes/vertex_main.glsl
+++ assets/shaders/includes/vertex_main.glsl
@@ -11,7 +11,8 @@ void main() {
   v_ScenePos = _Matrix_M * vec4(in_Position, 1.0);
   v_Normal = normalize(_Matrix_Normal * in_Normal);
   vec3 T = normalize(_Matrix_Normal * in_Tangent);
-  vec3 B = normalize(cross(v_Normal, T));
+  // Bitangent is negative because OpenGL's Y coordinate for images is reversed.
+  vec3 B = -normalize(cross(v_Normal, T));
   v_TBN = mat3(T, B, v_Normal);
 
   gl_Position = vert();

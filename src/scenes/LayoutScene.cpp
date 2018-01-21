409c00835fd754c04cd44352d3d396ed7c88b923
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-01-21 16:10:32 -0500

    [Layout] Create checkerboard with two spheres for Layout scene.

--- src/scenes/LayoutScene.cpp
+++ src/scenes/LayoutScene.cpp
@@ -5,11 +5,9 @@
 #include <scenes/LayoutScene.h>
 
 #include <glm/glm.hpp>
-#include <EngineTime.h>
-#include <forward_list>
 #include <materials/StandardMaterial.h>
 #include <behaviors/KeyboardCameraController.h>
-#include <lights/PointLight.h>
+#include <lights/DirectionalLight.h>
 
 std::unique_ptr<dg::LayoutScene> dg::LayoutScene::Make() {
   return std::unique_ptr<dg::LayoutScene>(new dg::LayoutScene());
@@ -21,75 +19,61 @@ void dg::LayoutScene::Initialize() {
   // Lock window cursor to center.
   window->LockCursor();
 
-  // Create textures.
-  std::shared_ptr<Texture> crateTexture = std::make_shared<Texture>(
-      Texture::FromPath("assets/textures/container2.png"));
-  std::shared_ptr<Texture> crateSpecularTexture = std::make_shared<Texture>(
-      Texture::FromPath("assets/textures/container2_specular.png"));
-  std::shared_ptr<Texture> hardwoodTexture = std::make_shared<Texture>(
-      Texture::FromPath("assets/textures/hardwood.jpg"));
-  std::shared_ptr<Texture> skyboxTexture = std::make_shared<Texture>(
-      Texture::FromPath("assets/textures/skybox_daylight.png"));
-
   // Create skybox.
-  skybox = std::unique_ptr<Skybox>(new Skybox(skyboxTexture));
+  skybox = std::unique_ptr<Skybox>(new Skybox(std::make_shared<Texture>(
+          Texture::FromPath("assets/textures/skybox_daylight.png"))));
 
-  // Create ceiling light source.
-  auto ceilingLight = std::make_shared<PointLight>(
+  // Create directinal light.
+  auto directionalLight = std::make_shared<DirectionalLight>(
+      glm::normalize(glm::vec3(0, -1, -0.3)),
       glm::vec3(1.0f, 0.93f, 0.86f),
       0.732f, 0.399f, 0.968f);
-  ceilingLight->transform.translation = glm::vec3(0.8f, 1.2f, -0.2f);
-  AddChild(ceilingLight);
-
-  // Create light cube material.
-  StandardMaterial lightMaterial = StandardMaterial::WithColor(
-      ceilingLight->specular);
-  lightMaterial.SetLit(false);
-
-  // Create light cube.
-  auto lightModel = std::make_shared<Model>(
-      dg::Mesh::Cube,
-      std::make_shared<StandardMaterial>(lightMaterial),
-      Transform::S(glm::vec3(0.05f)));
-  ceilingLight->AddChild(lightModel, false);
-
-  // Create wooden cube material.
-  StandardMaterial cubeMaterial = StandardMaterial::WithTexture(crateTexture);
-  cubeMaterial.SetSpecular(crateSpecularTexture);
-  cubeMaterial.SetShininess(64);
-
-  // Create wooden cube.
-  auto cube = std::make_shared<Model>(
-      dg::Mesh::Cube,
-      std::make_shared<StandardMaterial>(cubeMaterial),
-      Transform::TS(glm::vec3(0, 0.25f, 0), glm::vec3(0.5f)));
-  AddChild(cube);
+  AddChild(directionalLight);
 
   // Create floor material.
-  const int floorSize = 10;
-  StandardMaterial floorMaterial = StandardMaterial::WithTexture(
-      hardwoodTexture);
-  floorMaterial.SetUVScale(glm::vec2(floorSize));
+  glm::vec2 floorSize(12, 28);
+  Material floorMaterial;
+  floorMaterial.shader = std::make_shared<Shader>(Shader::FromFiles(
+        "assets/shaders/checkerboard.v.glsl",
+        "assets/shaders/checkerboard.f.glsl"));
+  floorMaterial.SetProperty("_Size", floorSize);
+  floorMaterial.SetProperty("_Colors[0]", glm::vec3(1, 0, 0));
+  floorMaterial.SetProperty("_Colors[1]", glm::vec3(1, 1, 0));
 
   // Create floor plane.
   AddChild(std::make_shared<Model>(
         dg::Mesh::Quad,
-        std::make_shared<StandardMaterial>(floorMaterial),
+        std::make_shared<Material>(floorMaterial),
         Transform::RS(
           glm::quat(glm::radians(glm::vec3(-90, 0, 0))),
-          glm::vec3(floorSize, floorSize, 1))));
+          glm::vec3(floorSize.x, floorSize.y, 1))));
+
+  // Create sphere material.
+  StandardMaterial sphereMaterial = StandardMaterial::WithColor(glm::vec3(0.3));
+  sphereMaterial.SetSpecular(0.3);
+  sphereMaterial.SetShininess(64);
+
+  // Create front sphere.
+  AddChild(std::make_shared<Model>(
+      dg::Mesh::Sphere,
+      std::make_shared<StandardMaterial>(sphereMaterial),
+      Transform::TS(glm::vec3(-3, 2, 0), glm::vec3(2.5))));
+
+  // Create back sphere.
+  AddChild(std::make_shared<Model>(
+      dg::Mesh::Sphere,
+      std::make_shared<StandardMaterial>(sphereMaterial),
+      Transform::TS(glm::vec3(-1, 1.5, -2), glm::vec3(2.5))));
 
   // Create camera.
   mainCamera = std::make_shared<Camera>();
-  mainCamera->transform.translation = glm::vec3(-1.25f, 2, 1.1f);
-  mainCamera->LookAtPoint(
-      (cube->transform.translation +
-       ceilingLight->transform.translation) / 2.f);
+  mainCamera->transform.translation = glm::vec3(-3, 2, 5);
+  mainCamera->LookAtPoint(glm::vec3(-3, 0, -7));
   AddChild(mainCamera);
 
   // Allow camera to be controller by the keyboard and mouse.
   Behavior::Attach(
       mainCamera,
-      std::make_shared<KeyboardCameraController>(window));
+      std::make_shared<KeyboardCameraController>(window, 4));
 }
 

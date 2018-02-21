a6fd83d93e6b4da3c4666ca6b8299429e6d20d45
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-02-20 21:54:00 -0500

    Add multiple togglable lights to DeepCloningScene.

--- include/scenes/DeepCloningScene.h
+++ include/scenes/DeepCloningScene.h
@@ -28,7 +28,7 @@ namespace dg {
     private:
 
       static std::shared_ptr<SceneObject> BuildWidget(
-          std::shared_ptr<Window> window);
+          std::shared_ptr<Window> window, Key key, glm::vec3 color);
 
   }; // class DeepCloningScene
 
@@ -43,11 +43,16 @@ namespace dg {
             std::weak_ptr<Model> button, std::weak_ptr<Light> light);
 
         Key key;
+        glm::vec3 color;
 
         virtual void Start();
         virtual void Update();
 
+        void SetLight(bool enabled);
+
       private:
+
+        glm::vec3 originalButtonPos;
 
         std::weak_ptr<Window> window;
         std::weak_ptr<Model> button;
--- src/scenes/DeepCloningScene.cpp
+++ src/scenes/DeepCloningScene.cpp
@@ -38,12 +38,12 @@ void dg::DeepCloningScene::Initialize() {
   // Create sky light.
   auto skyLight = std::make_shared<DirectionalLight>(
       glm::vec3(1.0f, 0.93f, 0.86f),
-      0.072f, 0.31f, 0.077f);
+      0.012f, 0.21f, 0.077f);
   skyLight->LookAtDirection(glm::normalize(glm::vec3(-0.3f, -1, -0.2f)));
   AddChild(skyLight);
 
   // Create floor material.
-  const int floorSize = 10;
+  const int floorSize = 500;
   StandardMaterial floorMaterial = StandardMaterial::WithTexture(
       std::make_shared<Texture>(Texture::FromPath(
           "assets/textures/Flooring_Stone_001/Flooring_Stone_001_COLOR.png")));
@@ -66,8 +66,8 @@ void dg::DeepCloningScene::Initialize() {
           glm::vec3(floorSize, floorSize, 1))));
 
   // Configure camera.
-  mainCamera->transform.translation = glm::vec3(-1.25f, 2, 1.1f);
-  mainCamera->LookAtPoint({ 0, 0, 0 });
+  mainCamera->transform.translation = glm::vec3(-2.85f, 3.56f, 4.16f);
+  mainCamera->LookAtDirection({ 0.30847f, -0.6647f, -0.6805f });
 
   // Allow camera to be controller by the keyboard and mouse.
   Behavior::Attach(
@@ -75,26 +75,34 @@ void dg::DeepCloningScene::Initialize() {
       std::make_shared<KeyboardCameraController>(window));
 
   // Create widgets
-  auto widget = BuildWidget(window);
-  AddChild(widget);
+  const glm::vec3 colors[] = {
+    glm::vec3(148, 0, 211) / 255.f,
+    glm::vec3(75, 0, 130) / 255.f,
+    glm::vec3(0, 0, 255) / 255.f,
+    glm::vec3(0, 255, 0) / 255.f,
+    glm::vec3(255, 255, 0) / 255.f,
+    glm::vec3(255, 127, 0) / 255.f,
+    glm::vec3(255, 0, 0) / 255.f,
+  };
+  int numColors = sizeof(colors) / sizeof(colors[0]);
+  for (int i = 0; i < numColors; i++) {
+    auto widget = BuildWidget(window, Key::NUM_1 + i, colors[i]);
+    widget->transform = Transform::T({ i - (numColors * 0.5f), 0, 0 });
+    widget->GetBehavior<WidgetBehavior>()->SetLight(false);
+    AddChild(widget);
+  }
 }
 
 std::shared_ptr<dg::SceneObject> dg::DeepCloningScene::BuildWidget(
-    std::shared_ptr<Window> window) {
+    std::shared_ptr<Window> window, Key key, glm::vec3 color) {
   auto widget = std::make_shared<SceneObject>();
-
-  glm::vec3 color(0, 0, 1);
 
   static std::shared_ptr<StandardMaterial> baseMaterial =
     std::make_shared<StandardMaterial>(
-        //StandardMaterial::WithTexture(std::make_shared<Texture>(
-            //Texture::FromPath("assets/textures/rustyplate.jpg"))));
         StandardMaterial::WithColor(glm::vec3(0.8)));
 
   std::shared_ptr<StandardMaterial> buttonMaterial =
-    std::make_shared<StandardMaterial>(
-        StandardMaterial::WithColor(color * 1.0f));
-  //buttonMaterial->SetLit(false);
+    std::make_shared<StandardMaterial>();
 
   // Base of button
   widget->AddChild(std::make_shared<Model>(
@@ -112,7 +120,7 @@ std::shared_ptr<dg::SceneObject> dg::DeepCloningScene::BuildWidget(
   widget->AddChild(button, false);
 
   // Button light
-  auto light = std::make_shared<PointLight>(color, 0.1f, 0.633f, 0.6f);
+  auto light = std::make_shared<PointLight>();
   light->transform = Transform::T({ 0, 1, 0 }); // Local to button
   button->AddChild(light, false);
   Behavior::Attach(
@@ -120,11 +128,11 @@ std::shared_ptr<dg::SceneObject> dg::DeepCloningScene::BuildWidget(
       std::make_shared<KeyboardLightController>(window));
 
   // Logic
-  Behavior::Attach(
+  auto logic = Behavior::Attach(
       widget,
       std::make_shared<WidgetBehavior>(window, button, light));
-  widget->GetBehavior<WidgetBehavior>()->key = Key::SPACE;
-  // TODO: Actually just have Behavior::Attach() return itself.
+  logic->key = key;
+  logic->color = color;
 
   return widget;
 }
@@ -139,9 +147,17 @@ dg::DeepCloning::WidgetBehavior::WidgetBehavior(
 }
 
 void WidgetBehavior::Start() {
+  Behavior::Start();
+
+  std::shared_ptr<Model> button = this->button.lock();
+  if (!button) return;
+
+  originalButtonPos = button->transform.translation;
 }
 
 void WidgetBehavior::Update() {
+  Behavior::Update();
+
   std::shared_ptr<Window> window = this->window.lock();
   std::shared_ptr<Model> button = this->button.lock();
   std::shared_ptr<Light> light = this->light.lock();
@@ -150,6 +166,30 @@ void WidgetBehavior::Update() {
   if (window->IsKeyJustPressed(key)) {
     light->enabled = !light->enabled;
   }
+
+  if (window->IsKeyPressed(key)) {
+    button->transform.translation = originalButtonPos - (0.06f * UP);
+  } else if (light->enabled) {
+    button->transform.translation = originalButtonPos - (0.03f * UP);
+  } else {
+    button->transform.translation = originalButtonPos;
+  }
+
+  std::static_pointer_cast<StandardMaterial>(
+      button->material)->SetDiffuse(color);
+
+  if (light->enabled) {
+    light->SetAmbient(color * 0.1f);
+    light->SetDiffuse(color * 0.633f);
+    light->SetSpecular(color * 0.6f);
+  }
+}
+
+void WidgetBehavior::SetLight(bool enabled) {
+  std::shared_ptr<Light> light = this->light.lock();
+  if (!light) return;
+
+  light->enabled = enabled;
 }
 
 #pragma endregion

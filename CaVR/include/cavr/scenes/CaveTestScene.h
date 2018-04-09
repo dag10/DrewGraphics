33b20d0826b867238836f8540d8075c27c6ba336
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-04-09 01:47:25 -0400

    [CaVR] Started tunnel mesh-building logic. Visualizes knots and knot vertices.

--- CaVR/include/cavr/scenes/CaveTestScene.h
+++ CaVR/include/cavr/scenes/CaveTestScene.h
@@ -5,13 +5,76 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 #include "dg/Scene.h"
+
+namespace dg {
+  class SceneObject;
+  class Model;
+  class Mesh;
+  class Light;
+  class StandardMaterial;
+} // namespace dg
 
 namespace cavr {
 
-  class Model;
-  class Light;
-  class SpotLight;
+  class Tunnel {
+
+    public:
+
+      static const int VerticesPerRing = 12;
+
+      class Knot {
+
+        public:
+
+          // All units are in cave-local space.
+
+          Knot() = default;
+          Knot(glm::vec3 position, glm::vec3 forward, float radius);
+
+          inline const glm::vec3 &GetPosition() const { return position; }
+          inline const glm::vec3 &GetRight() const { return xf[0]; }
+          inline const glm::vec3 &GetUp() const { return xf[1]; }
+          inline glm::vec3 GetForward() const { return -xf[2]; }
+          inline const glm::mat3x3 &GetXF() const { return xf; }
+          inline float GetRadius() const { return radius; }
+          inline const glm::vec3 &GetVertexPosition(int index) const {
+            return vertices[index];
+          }
+
+         private:
+
+          glm::vec3 PositionForVertex(int index) const;
+
+          glm::vec3 position = glm::vec3(0);
+          glm::mat3x3 xf = glm::mat3x3(1);
+          float radius = 0;
+          glm::vec3 vertices[VerticesPerRing];
+      };
+
+      class Segment {
+
+        public:
+
+          Segment(std::shared_ptr<Knot> start, std::shared_ptr<Knot> end);
+
+          void CreateMesh();
+
+          inline std::shared_ptr<Knot> GetStartKnot() const { return start; }
+          inline std::shared_ptr<Knot> GetEndKnot() const { return end; }
+          inline std::shared_ptr<dg::Mesh> GetMesh() const { return mesh; }
+
+        public:
+
+          std::shared_ptr<Knot> start;
+          std::shared_ptr<Knot> end;
+          std::shared_ptr<dg::Mesh> mesh = nullptr;
+
+      };
+
+      std::vector<Segment> segments;
+  };
 
   class CaveTestScene : public dg::Scene {
 
@@ -26,7 +89,17 @@ namespace cavr {
 
       CaveTestScene();
 
+      void CreateCave();
+      std::shared_ptr<dg::SceneObject> CreateKnotVertexModel(
+const Tunnel::Knot &knot) const;
+      std::shared_ptr<dg::Model> CreateKnotModel(
+          const Tunnel::Knot &knot) const;
+
       std::shared_ptr<dg::Light> skyLight;
+
+      std::shared_ptr<dg::StandardMaterial> segmentMaterial;
+      std::shared_ptr<dg::StandardMaterial> knotVertexMaterial;
+      std::shared_ptr<dg::StandardMaterial> knotMaterial;
 
   }; // class CaveTestScene
 
--- CaVR/src/scenes/CaveTestScene.cpp
+++ CaVR/src/scenes/CaveTestScene.cpp
@@ -84,6 +84,129 @@ void cavr::CaveTestScene::Initialize() {
     dg::Behavior::Attach(
         mainCamera, std::make_shared<dg::KeyboardCameraController>(window));
   }
+
+  // Create knot material for visualization.
+  knotMaterial = std::make_shared<dg::StandardMaterial>(
+      dg::StandardMaterial::WithTransparentColor(
+          glm::vec4(0.5f, 0.1f, 0.1f, 0.4f)));
+  knotMaterial->rasterizerOverride = dg::RasterizerState::AdditiveBlending();
+
+  // Create knot vertex material for visualization.
+  knotVertexMaterial = std::make_shared<dg::StandardMaterial>(
+      dg::StandardMaterial::WithTransparentColor(
+          glm::vec4(0.4f, 0.4f, 0.9f, 0.9f)));
+  knotVertexMaterial->rasterizerOverride = dg::RasterizerState::AdditiveBlending();
+
+  // Create segment material for visualization.
+  segmentMaterial = std::make_shared<dg::StandardMaterial>(
+      dg::StandardMaterial::WithTransparentColor(
+          glm::vec4(0.1f, 0.5f, 0.1f, 0.3f)));
+  segmentMaterial->rasterizerOverride = dg::RasterizerState::AdditiveBlending();
+  segmentMaterial->rasterizerOverride.SetCullMode(
+      dg::RasterizerState::CullMode::OFF);
+
+  CreateCave();
+}
+
+void cavr::CaveTestScene::CreateCave() {
+  // Cave container.
+  auto cave = std::make_shared<dg::SceneObject>();
+  AddChild(cave);
+
+  // This tunnel's characteristics.
+  const glm::vec3 segmentStart(-1.f, 1.2f, -0.3f);
+  const glm::vec3 segmentEnd(1.f, 0.5f, 0.3f);
+  const int numKnots = 5;
+  const glm::vec3 segmentInterval =
+      (segmentEnd - segmentStart) / (float)numKnots;
+  const float minRadius = 0.15f;
+  const float maxRadius = 0.25f;
+
+  // Create the tunnel's knots.
+  std::vector<std::shared_ptr<Tunnel::Knot>> knots;
+  for (int i = 0; i < numKnots; i++) {
+    float radius;
+    if (i == 0) {
+      radius = maxRadius;
+    } else if (i == numKnots - 1) {
+      radius = minRadius;
+    } else {
+      float pct = (float)(i - 1) / (float)(numKnots - 3);
+      radius = maxRadius + (pct * (minRadius - maxRadius));
+    }
+    glm::vec3 pos = segmentStart + (segmentInterval * (float)i);
+    glm::vec3 forward = glm::normalize(segmentInterval);
+    knots.push_back(std::make_shared<Tunnel::Knot>(pos, forward, radius));
+  }
+
+  // Visualize each knot as a cylinder.
+  for (auto &knot : knots) {
+    cave->AddChild(CreateKnotModel(*knot), false);
+  }
+
+  // Create segments for each adjacent pair of knots.
+  Tunnel tunnel;
+  for (int i = 0; i < numKnots - 1; i++) {
+    tunnel.segments.push_back(Tunnel::Segment(knots[i], knots[i + 1]));
+  }
+
+  // Visualize vertex positions for second segment.
+  cave->AddChild(CreateKnotVertexModel(*tunnel.segments[1].start), false);
+  cave->AddChild(CreateKnotVertexModel(*tunnel.segments[1].end), false);
+
+  // Visualize second segment.
+  tunnel.segments[1].CreateMesh();
+}
+
+std::shared_ptr<dg::SceneObject> cavr::CaveTestScene::CreateKnotVertexModel(
+    const Tunnel::Knot &knot) const {
+  auto container = std::make_shared<SceneObject>();
+  for (int i = 0; i < Tunnel::VerticesPerRing; i++) {
+    container->AddChild(
+        std::make_shared<dg::Model>(
+            dg::Mesh::Sphere, knotVertexMaterial,
+            dg::Transform::TS(knot.GetVertexPosition(i), glm::vec3(0.01f))),
+        false);
+  }
+  return container;
+}
+
+std::shared_ptr<dg::Model> cavr::CaveTestScene::CreateKnotModel(
+    const Tunnel::Knot &knot) const {
+  return std::make_shared<dg::Model>(
+      dg::Mesh::Cylinder, knotMaterial,
+      dg::Transform::TRS(
+          knot.GetPosition(),
+          glm::quat(knot.GetXF()) *
+              glm::quat(glm::radians(glm::vec3(90, 0, 0))),
+          glm::vec3(knot.GetRadius() * 2.f, 0.02f, knot.GetRadius() * 2.f)));
+}
+
+cavr::Tunnel::Knot::Knot(glm::vec3 position, glm::vec3 forward, float radius)
+    : position(position), radius(radius) {
+  glm::vec3 right = glm::normalize(glm::cross(forward, dg::UP));
+  glm::vec3 up = glm::normalize(glm::cross(right, forward));
+  xf = glm::mat3x3(right, up, -forward);
+  for (int i = 0; i < Tunnel::VerticesPerRing; i++) {
+    vertices[i] = PositionForVertex(i);
+  }
+}
+
+glm::vec3 cavr::Tunnel::Knot::PositionForVertex(int index) const {
+  return position + glm::quat(xf) *
+                        glm::quat(glm::radians(
+                            glm::vec3(0, 0, index * 360 / VerticesPerRing))) *
+                        glm::vec3(radius, 0, 0);
+}
+
+cavr::Tunnel::Segment::Segment(std::shared_ptr<Knot> start,
+                               std::shared_ptr<Knot> end)
+    : start(start), end(end) {}
+
+void cavr::Tunnel::Segment::CreateMesh() {
+  for (int i = 0; i < VerticesPerRing; i++) {
+    // TODO
+  }
 }
 
 void cavr::CaveTestScene::Update() {

413ab8ec9b44bdf67fa56af74906dd4f20d10082
Drew Gottlieb <gottlieb.drew@gmail.com>
2018-05-06 20:26:56 -0400

    AOScene now has overlays to eventually show GBuffer.

--- Experiments/include/dg/scenes/AOScene.h
+++ Experiments/include/dg/scenes/AOScene.h
@@ -5,9 +5,12 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 #include "dg/Scene.h"
 
 namespace dg {
+
+  class Model;
 
   class AOScene : public Scene {
 
@@ -18,6 +21,26 @@ namespace dg {
       AOScene();
 
       virtual void Initialize();
+      virtual void Update();
+
+    private:
+
+      enum class OverlayState {
+        None,
+        GBuffer,
+      };
+
+      void InitializeSubrenders();
+      void CreateGBuffer();
+
+      virtual void RenderFramebuffers();
+      virtual void PreRender();
+
+      OverlayState overlayState = OverlayState::GBuffer;
+
+      Subrender geometrySubrender;
+
+      std::vector<std::shared_ptr<Model>> overlayQuads;
 
   }; // class AOScene
 
--- Experiments/src/scenes/AOScene.cpp
+++ Experiments/src/scenes/AOScene.cpp
@@ -17,6 +17,7 @@
 #include "dg/Window.h"
 #include "dg/behaviors/KeyboardCameraController.h"
 #include "dg/behaviors/KeyboardLightController.h"
+#include "dg/materials/ScreenQuadMaterial.h"
 #include "dg/materials/StandardMaterial.h"
 #include "dg/materials/UVMaterial.h"
 
@@ -84,4 +85,83 @@ void dg::AOScene::Initialize() {
   // Allow camera to be controller by the keyboard and mouse.
   Behavior::Attach(cameras.main,
                    std::make_shared<KeyboardCameraController>(window));
+
+  // Create screen space quads to visualize internal textures.
+  for (int i = 0; i < 3; i++) {
+    auto quad = std::make_shared<Model>(
+        Mesh::Quad,
+        std::make_shared<ScreenQuadMaterial>(glm::vec3(1, 0, 1), glm::vec2(1)),
+        Transform());
+    overlayQuads.push_back(quad);
+    AddChild(quad);
+  }
+
+  InitializeSubrenders();
+}
+
+void dg::AOScene::InitializeSubrenders() {
+  geometrySubrender.type = Subrender::Type::MonoscopicWindow;
+  geometrySubrender.renderSkybox = false;
+}
+
+void dg::AOScene::CreateGBuffer() {
+  FrameBuffer::Options opts;
+  opts.width = window->GetWidth();
+  opts.height = window->GetHeight();
+  opts.depthReadable = true;
+  opts.hasColor = true;
+  opts.hasStencil = false;
+  geometrySubrender.framebuffer = FrameBuffer::Create(opts);
+}
+
+void dg::AOScene::PreRender() {
+  // Always render geometry pass with whichever camera we're about to do
+  // the main render with.
+  geometrySubrender.camera = subrenders.main.camera;
+}
+
+void dg::AOScene::RenderFramebuffers() {
+  // Create or resize geometry framebuffer if needed.
+  if (geometrySubrender.framebuffer == nullptr ||
+      geometrySubrender.framebuffer->GetWidth() != window->GetWidth() ||
+      geometrySubrender.framebuffer->GetHeight() != window->GetHeight()) {
+    CreateGBuffer();
+  }
+
+  PerformSubrender(geometrySubrender);
+}
+
+void dg::AOScene::Update() {
+  Scene::Update();
+
+  // G key switches to GBuffer overlay state.
+  if (window->IsKeyJustPressed(Key::G)) {
+    overlayState = OverlayState::GBuffer;
+  }
+
+  // N key switches to no overlay state.
+  if (window->IsKeyJustPressed(Key::N)) {
+    overlayState = OverlayState::None;
+  }
+
+  // Update overlay quads based on overlay state.
+  for (auto &quad : overlayQuads) {
+    quad->enabled = false;
+  }
+  switch (overlayState) {
+    case OverlayState::None:
+      break;
+    case OverlayState::GBuffer: {
+      glm::vec2 quadScale =
+          glm::vec2(1) / glm::vec2(window->GetAspectRatio(), 1);
+      overlayQuads[0]->enabled = true;
+      std::static_pointer_cast<ScreenQuadMaterial>(overlayQuads[0]->material)
+          ->SetScale(quadScale);
+      std::static_pointer_cast<ScreenQuadMaterial>(overlayQuads[0]->material)
+          ->SetOffset(glm::vec2(1 - quadScale.x * 0.5, quadScale.y * 0.5));
+      //std::static_pointer_cast<ScreenQuadMaterial>(overlayQuads[0]->material)
+          //->SetTexture(geometrySubrender.framebuffer->GetDepthTexture());
+      break;
+    };
+  }
 }
